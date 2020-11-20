5e91e2f9ca915dbf03d49b4a135cea4a086e335f
Magic <cardhousemagic@gmail.com>
2020-11-19 22:41:20 -0500

    Refactored the RedemptionList into its own component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Redemption from './Redemption'
+import RedemptionList from './RedemptionList'
 import './App.css';
 
 class App extends React.Component {
@@ -7,14 +7,10 @@ class App extends React.Component {
     super(props);
     this.state = {
       redemptions: [],
-      newRedemption: {
-        name: '',
-        timer: null
-      },
     };
   }
 
-  removeRedemption(id) {
+  handleRemoveRedemption(id) {
     var redemptions = this.state.redemptions;
     delete redemptions[id];
     this.setState({
@@ -22,53 +18,24 @@ class App extends React.Component {
     });
   }
 
-  addRedemption() {
-    const nameInput = document.getElementById('redemption_name');
-    const timerInput = document.getElementById('redemption_timer');
+  handleAddRedemption(name, timer) {
     this.setState({
       redemptions: this.state.redemptions.concat([{
-        title: nameInput.value,
-        timer: (timerInput.value === "") ? 0 : timerInput.value
+        title: name,
+        timer: (timer === "") ? 0 : timer
       }]),
     });
-    nameInput.value = '';
-    timerInput.value = 0;
-  }
-
-  renderRedemption(redemption, index) {
-    return (
-        <Redemption 
-          title={redemption.title} 
-          id={index} handleRemove={(index) => this.removeRedemption(index)} 
-          timer={redemption.timer}
-        />
-      ) 
   }
 
   render() {
-    const redemptions = this.state.redemptions.slice();
-    const redemptionList = redemptions.map((redem, index) => <li key={index}>{this.renderRedemption(redem, index)}</li>);
     return(
       <div>
         <h1>Redemptions</h1>
-        <ol>{redemptionList}</ol>
-        <div>
-        <label htmlFor="redemption_name">Name:</label>
-          <input 
-            id="redemption_name"
-            type="text" 
-            defaultValue=""
-            placeholder="Name of redemption" />
-            <br />
-          <label htmlFor="redemption_timer">Timer:</label>
-          <input 
-            id="redemption_timer"
-            type="number" 
-            defaultValue="0"
-            placeholder="Number of minutes" />
-            <br />
-          <button onClick={() => this.addRedemption()}>Add redemption</button>
-        </div>
+        <RedemptionList 
+          redemptions={this.state.redemptions} 
+          add={(name, timer) => this.handleAddRedemption(name, timer)} 
+          remove={(id) => this.handleRemoveRedemption(id)} 
+        />
       </div>
     );
   }
---
+++ src/RedemptionList.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import Redemption from './Redemption'
+
+export default class RedemptionList extends React.Component {
+	renderRedemption(redemption, index) {
+		return (
+			<Redemption 
+				title={redemption.title} 
+				id={index} 
+				handleRemove={(index) => this.props.remove(index)} 
+				timer={redemption.timer}
+			/>
+		) 
+	}
+
+	handleAdd() {
+		const nameInput = document.getElementById('redemption_name');
+    	const timerInput = document.getElementById('redemption_timer');
+    	this.props.add(nameInput.value, timerInput.value);
+    	nameInput.value = '';
+    	timerInput.value = 0;
+	}
+
+	renderRedemptions() {
+		return this.props.redemptions.map(
+			(redem, index) => (
+				<li key={index}>{this.renderRedemption(redem, index)}</li>
+			)
+		);
+	}
+
+	render() {
+		return (
+			<div>
+				<ol>{this.renderRedemptions()}</ol>
+				<div>
+		          <label htmlFor="redemption_name">Name:</label>
+		          <input 
+		            id="redemption_name"
+		            type="text" 
+		            defaultValue=""
+		            placeholder="Name of redemption" />
+		            <br />
+		          <label htmlFor="redemption_timer">Timer:</label>
+		          <input 
+		            id="redemption_timer"
+		            type="number" 
+		            defaultValue="0"
+		            placeholder="Number of minutes" />
+		            <br />
+		          <button onClick={() => this.handleAdd()}>Add redemption</button>
+		        </div>
+			</div>
+		)
+	}
+}
